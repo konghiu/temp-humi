3ebf8f6b6dae9e99c23c3c0edec756987fffd8de
konghiu <conghieucoconcuto@gmail.com>
2025-07-15 00:32:20 +0700

    commit 5

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    res.header("Content-Type", "application/json;charset=UTF-8");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use("/data", dataRoute);
 
 app.listen(PORT, (error) => {
