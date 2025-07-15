ff067178f56d151f192a88a0bf339c5fe7d05a0f
konghiu <conghieucoconcuto@gmail.com>
2025-07-15 15:41:11 +0700

    commit 10

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
 export let status = "ok";
+const clientOptions = {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 
 async function connect() {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
         status = "alright";
         console.log("connect successfull");
     } catch (err) {
         console.log("connect failure");
         status = "error";
+        connect();
     }
 }
 
